21681110ca83b2865350167910e5d8f825a024fe
brooks <bpoltl1@gmail.com>
2018-12-01 05:38:07 -0600

    added error to state and rendered it to dom

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,21 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            latitude:''
+            lat:null,
+            errorMessage: ''
         }
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({errorMessage: err.message})
+        );
+        
     }
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({latitude: position.coords.latitude}),
-            err => console.log(err)
-        );
-        return <div>latitude: {this.state.latitude}</div>
+        return (
+        <div>
+            latitude: {this.state.lat}<br />
+            Error: {this.state.errorMessage}
+        </div>
+        )
     }
 }
-
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
