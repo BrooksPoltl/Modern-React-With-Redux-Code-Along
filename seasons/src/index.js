4812ddea207cbd32fa2bf455fcbbfbb8c78326fe
brooks <bpoltl1@gmail.com>
2018-12-01 05:52:34 -0600

    refactored the geolocation to the componentDidMount lifecycle method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,13 +8,13 @@ class App extends React.Component {
             lat:null,
             errorMessage: ''
         }
-
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({errorMessage: err.message})
-        );
-        
     }
+        componentDidMount(){
+            window.navigator.geolocation.getCurrentPosition(
+                position => this.setState({ lat: position.coords.latitude }),
+                err => this.setState({ errorMessage: err.message })
+            );
+        }
     render(){
     if(this.state.errorMessage && !this.state.lat){
         return <div>Error: {this.state.errorMessage}</div>
