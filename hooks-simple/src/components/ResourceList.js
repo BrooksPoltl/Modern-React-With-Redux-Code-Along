a60a9674a4bd62e03219f6f9801c54956dc7e514
brooks <bpoltl1@gmail.com>
2019-03-03 18:52:58 -0600

    refactor to function

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,22 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component{
-    state = {resources: []}
-    async componentDidMount(){
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+const ResourceList =(props)=>{
+    const [resources, setResources] = useState([]);
+
+    const fetchResource = async ()=>{
+        const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+        )
         this.setState({resources: response.data})
     }
-    async componentDidUpdate(prevProps){
-        if(prevProps.resource !== this.props.resource){
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-            this.setState ({resources: response.data});
+        return <div>{resources.length}</div>
 
-        }
-      
-    }
-    render(){
-        return <div>{this.state.resources.length}</div>
-    }
 }
 export default ResourceList
