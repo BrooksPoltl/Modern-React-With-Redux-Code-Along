aba2d84fa59ff2e5f1e5440c57ca062181ab14b6
brooks <bpoltl1@gmail.com>
2019-03-03 19:19:57 -0600

    extracted out state and useEffect

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,21 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const ResourceList =({resource})=>{
-    const [resources, setResources] = useState([]);
+const useResources = (resource) =>{
+    const [resources, setResources] = useState ([]);
 
-    const fetchResource = async ()=>{
-        const response = await axios.get(
+useEffect (
+  () => {
+    (async resource => {
+      const response = await axios.get (
         `https://jsonplaceholder.typicode.com/${resource}`
-        )
-        setResources(response.data)
-    }
-    useEffect(
-        ()=>{
-            fetchResource(resource)
-        },
-        [resource]
-    )
+      );
+
+      setResources (response.data);
+    }) (resource);
+  },
+  [resource]
+    );
+  return resources;
+}
+
+const ResourceList =({resource})=>{
+    const resources = useResources(resource)
         return (
             <ul>{
                 resources.map(record =>(
